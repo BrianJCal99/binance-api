cef5b771312b7600b6aa0d12de33417770911bf1
Brian Caldera <88503893+BrianJCal99@users.noreply.github.com>
2025-01-26 16:15:22 +1100

    Update page.js
    add all charts

--- src/app/charts/[param]/page.js
+++ src/app/charts/[param]/page.js
@@ -7,33 +7,84 @@ import { createChart } from "lightweight-charts";
 export default function Home() {
   const params = useParams();
   const { param } = params;
+
   const candlestickChartRef = useRef(null); // Reference to the chart container
   const areaChartRef = useRef(null); // Reference to the chart container
+  const barChartRef = useRef(null); // Reference to the chart container
+  const baselineChartRef = useRef(null); // Reference to the chart container
+  const histogramChartRef = useRef(null); // Reference to the chart container
+  const lineChartRef = useRef(null); // Reference to the chart container
+
   const candlestickSeriesRef = useRef(null); // Reference to the candlestick series
-  const areaSeriesRef = useRef(null); // Reference to the candlestick series
+  const areaSeriesRef = useRef(null); // Reference to the area series
+  const barSeriesRef = useRef(null); // Reference to the bar series
+  const baselineSeriesRef = useRef(null); // Reference to the bar series
+  const histogramSeriesRef = useRef(null); // Reference to the bar series
+  const lineSeriesRef = useRef(null); // Reference to the bar series
+
   const [ticker, setTicker] = useState({}); // State to store data for all symbols
   const [kline, setKline] = useState({}); // State to store data for all symbols
 
+  const [priceMovementState, setPriceMovementState] = useState(null); // State to store price movement state
+
+  // Charts
   useEffect(() => {
-    const candlestickChart = createChart(areaChartRef.current, {
+    // Candlestick
+    const candlestickChart = createChart(candlestickChartRef.current, {
       width: 1200,
       height: 400,
+      layout: {
+        background: { type: "solid", color: "black" }, // Dark background color
+        textColor: "#d1d4dc", // Light text color
+      },
+      grid: {
+        vertLines: {
+          color: "#2c2c2c", // Vertical grid lines color
+        },
+        horzLines: {
+          color: "#2c2c2c", // Horizontal grid lines color
+        },
+      },
+      priceScale: {
+        borderColor: "#555", // Color of the border for the price scale
+      },
+      timeScale: {
+        borderColor: "#555", // Color of the border for the time scale
+      },
     });
 
-    // Add Candlestick Series to Chart
     const candlestickSeries = candlestickChart.addCandlestickSeries({
       upColor: "#26a69a",
       downColor: "#ef5350",
-      borderVisible: true,
+      borderVisible: false,
       wickUpColor: "#26a69a",
       wickDownColor: "#ef5350",
     });
     candlestickSeriesRef.current = candlestickSeries;
-    candlestickChart.timeScale().fitContent();
+    candlestickChart.timeScale().applyOptions({ timeVisible: true });
 
-    const areaChart = createChart(candlestickChartRef.current, {
+    // Area
+    const areaChart = createChart(areaChartRef.current, {
       width: 1200,
       height: 400,
+      layout: {
+        background: { type: "solid", color: "black" }, // Dark background color
+        textColor: "#d1d4dc", // Light text color
+      },
+      grid: {
+        vertLines: {
+          color: "#2c2c2c", // Vertical grid lines color
+        },
+        horzLines: {
+          color: "#2c2c2c", // Horizontal grid lines color
+        },
+      },
+      priceScale: {
+        borderColor: "#555", // Color of the border for the price scale
+      },
+      timeScale: {
+        borderColor: "#555", // Color of the border for the time scale
+      },
     });
 
     const areaSeries = areaChart.addAreaSeries({
@@ -42,7 +93,137 @@ export default function Home() {
       bottomColor: "rgba(41, 98, 255, 0.28)",
     });
     areaSeriesRef.current = areaSeries;
-    areaChart.timeScale().fitContent();
+    areaChart.timeScale().applyOptions({ timeVisible: true });
+
+    // Bar
+    const barChart = createChart(barChartRef.current, {
+      width: 1200,
+      height: 400,
+      layout: {
+        background: { type: "solid", color: "black" }, // Dark background color
+        textColor: "#d1d4dc", // Light text color
+      },
+      grid: {
+        vertLines: {
+          color: "#2c2c2c", // Vertical grid lines color
+        },
+        horzLines: {
+          color: "#2c2c2c", // Horizontal grid lines color
+        },
+      },
+      priceScale: {
+        borderColor: "#555", // Color of the border for the price scale
+      },
+      timeScale: {
+        borderColor: "#555", // Color of the border for the time scale
+      },
+    });
+
+    const barSeries = barChart.addBarSeries({
+      upColor: "#26a69a",
+      downColor: "#ef5350",
+      borderVisible: true,
+      wickUpColor: "#26a69a",
+      wickDownColor: "#ef5350",
+    });
+    barSeriesRef.current = barSeries;
+    barChart.timeScale().applyOptions({ timeVisible: true });
+
+    // Baseline
+    const baselineChart = createChart(baselineChartRef.current, {
+      width: 1200,
+      height: 400,
+      layout: {
+        background: { type: "solid", color: "black" }, // Dark background color
+        textColor: "#d1d4dc", // Light text color
+      },
+      grid: {
+        vertLines: {
+          color: "#2c2c2c", // Vertical grid lines color
+        },
+        horzLines: {
+          color: "#2c2c2c", // Horizontal grid lines color
+        },
+      },
+      priceScale: {
+        borderColor: "#555", // Color of the border for the price scale
+      },
+      timeScale: {
+        borderColor: "#555", // Color of the border for the time scale
+      },
+    });
+
+    const baselineSeries = baselineChart.addBaselineSeries({
+      baseValue: { type: "price", price: 25 },
+      topLineColor: "rgba( 38, 166, 154, 1)",
+      topFillColor1: "rgba( 38, 166, 154, 0.28)",
+      topFillColor2: "rgba( 38, 166, 154, 0.05)",
+      bottomLineColor: "rgba( 239, 83, 80, 1)",
+      bottomFillColor1: "rgba( 239, 83, 80, 0.05)",
+      bottomFillColor2: "rgba( 239, 83, 80, 0.28)",
+    });
+    baselineSeriesRef.current = baselineSeries;
+    baselineChart.timeScale().applyOptions({ timeVisible: true });
+
+    // Histogram
+    const histogramChart = createChart(histogramChartRef.current, {
+      width: 1200,
+      height: 400,
+      layout: {
+        background: { type: "solid", color: "black" }, // Dark background color
+        textColor: "#d1d4dc", // Light text color
+      },
+      grid: {
+        vertLines: {
+          color: "#2c2c2c", // Vertical grid lines color
+        },
+        horzLines: {
+          color: "#2c2c2c", // Horizontal grid lines color
+        },
+      },
+      priceScale: {
+        borderColor: "#555", // Color of the border for the price scale
+      },
+      timeScale: {
+        borderColor: "#555", // Color of the border for the time scale
+      },
+    });
+
+    const histogramSeries = histogramChart.addHistogramSeries({
+      color: "#26a69a",
+    });
+    histogramSeriesRef.current = histogramSeries;
+    histogramChart.timeScale().applyOptions({ timeVisible: true });
+
+    // Area
+    const lineChart = createChart(lineChartRef.current, {
+      width: 1200,
+      height: 400,
+      layout: {
+        background: { type: "solid", color: "black" }, // Dark background color
+        textColor: "#d1d4dc", // Light text color
+      },
+      grid: {
+        vertLines: {
+          color: "#2c2c2c", // Vertical grid lines color
+        },
+        horzLines: {
+          color: "#2c2c2c", // Horizontal grid lines color
+        },
+      },
+      priceScale: {
+        borderColor: "#555", // Color of the border for the price scale
+      },
+      timeScale: {
+        borderColor: "#555", // Color of the border for the time scale
+      },
+    });
+
+    const lineSeries = lineChart.addLineSeries({
+      lineColor: "#2962FF",
+    });
+    lineSeriesRef.current = lineSeries;
+    lineChart.timeScale().applyOptions({ timeVisible: true });
 
     // Cleanup on unmount
     return () => {
@@ -50,15 +231,19 @@ export default function Home() {
     };
   }, []);
 
+  // Candlestick, Bar
   useEffect(() => {
     // Connect to Binance WebSocket
-    const binanceSocket = new WebSocket(
+    const binanceSocket_Kline = new WebSocket(
       `wss://stream.binance.com:9443/ws/${param}@kline_1s`
     );
 
-    binanceSocket.onmessage = (event) => {
+    binanceSocket_Kline.onopen = () => {
+      console.log("WebSocket connection for kline payload established.");
+    };
+
+    binanceSocket_Kline.onmessage = (event) => {
       const message = JSON.parse(event.data);
-      console.log(message);
       const { e: eventType, E: eventTime, s: symbol } = message;
       const {
         c: closePrice,
@@ -85,7 +270,7 @@ export default function Home() {
         },
       }));
 
-      // Update chart data
+      // Update Candlestick chart data
       const candlestickData = {
         time: Math.floor(eventTime / 1000),
         open: parseFloat(openPrice),
@@ -95,27 +280,47 @@ export default function Home() {
       };
 
       if (candlestickSeriesRef.current) {
-        console.log(candlestickData);
         candlestickSeriesRef.current.update(candlestickData);
-        // candlestickSeriesRef.current.setKline([candlestickData]);
+      }
+
+      // Update Bar chart data
+      const barData = {
+        open: parseFloat(openPrice),
+        high: parseFloat(highPrice),
+        low: parseFloat(lowPrice),
+        close: parseFloat(closePrice),
+        time: Math.floor(eventTime / 1000),
+      };
+
+      if (barSeriesRef.current) {
+        barSeriesRef.current.update(barData);
       }
     };
 
     // Cleanup WebSocket on component unmount
     return () => {
-      binanceSocket.close();
+      binanceSocket_Kline.close();
     };
   }, [param]);
 
+  // Area, Baseline, Histogram, Line
   useEffect(() => {
     // Connect to Binance WebSocket
     const binanceSocket_Ticker = new WebSocket(
       `wss://stream.binance.com:9443/ws/${param}@ticker`
     );
 
+    binanceSocket_Ticker.onopen = () => {
+      console.log("WebSocket connection for ticker payload established.");
+    };
+
+    let previousLastPrice = null; // Initialize to null to indicate no prior price
+
+    let previousPriceChange = null;
+    let previousPriceMovementState = null;
+
     binanceSocket_Ticker.onmessage = (event) => {
       const message = JSON.parse(event.data);
-      console.log(message);
       const {
         e: eventType,
         E: eventTime,
@@ -124,6 +329,37 @@ export default function Home() {
         P: priceChangePercent,
         c: lastPrice,
       } = message;
+
+      // Determine the color for the histogram based on price comparison
+      const color =
+        previousLastPrice === null
+          ? "#26a69a" // Default color for the first update (green)
+          : lastPrice < previousLastPrice
+          ? "#ef5350" // Red for price drop
+          : "#26a69a"; // Green for price rise
+
+      // Update the ref for the next message
+      previousLastPrice = lastPrice;
+
+      // Determine price movement state (UP/DOWN)
+      let newPriceMovementState;
+      if (previousPriceChange === null) {
+        newPriceMovementState = null; // Initial state
+      } else {
+        if (priceChange === previousPriceChange) {
+          newPriceMovementState = previousPriceMovementState; // Retain previous state
+        } else {
+          newPriceMovementState =
+            priceChange < previousPriceChange ? "DOWN" : "UP";
+        }
+      }
+
+      // Update previous variables for next iteration
+      previousPriceChange = priceChange;
+      previousPriceMovementState = newPriceMovementState;
+
+      // Update the price movement state
+      setPriceMovementState(newPriceMovementState);
 
       // Update tickers state
       setTicker((prevTickers) => ({
@@ -137,16 +373,44 @@ export default function Home() {
         },
       }));
 
-      // Update chart data
+      // Update area chart data
       const areaSeriesData = {
-        time: Math.floor(eventTime / 1000),
         value: parseFloat(lastPrice),
+        time: Math.floor(eventTime / 1000),
       };
 
       if (areaSeriesRef.current) {
-        console.log(areaSeriesData);
         areaSeriesRef.current.update(areaSeriesData);
-        // areaSeriesRef.current.setTicker([areaSeriesData]);
+      }
+
+      // Update baseline chart data
+      const baselineSeriesData = {
+        value: parseFloat(lastPrice),
+        time: Math.floor(eventTime / 1000),
+      };
+
+      if (baselineSeriesRef.current) {
+        baselineSeriesRef.current.update(baselineSeriesData);
+      }
+
+      // Update histogram chart data
+      const histogramSeriesData = {
+        value: parseFloat(lastPrice),
+        time: Math.floor(eventTime / 1000),
+        color: color,
+      };
+
+      if (histogramSeriesRef.current) {
+        histogramSeriesRef.current.update(histogramSeriesData);
+      }
+      // Update line chart data
+      const lineSeriesData = {
+        value: parseFloat(lastPrice),
+        time: Math.floor(eventTime / 1000),
+      };
+
+      if (lineSeriesRef.current) {
+        lineSeriesRef.current.update(lineSeriesData);
       }
     };
 
@@ -157,7 +421,7 @@ export default function Home() {
   }, [param]);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-black text-white grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         {Object.keys(ticker).length > 0 && (
           <div className="">
@@ -166,17 +430,52 @@ export default function Home() {
             </h2>
             {Object.values(ticker).map((data, index) => (
               <div key={index} className="text-lg">
-                <div>US${parseFloat(data.lastPrice).toFixed(2)}</div>
-                {parseFloat(data.priceChange).toFixed(2)}
-                <span className="px-2">
-                  ({parseFloat(data.priceChangePercent).toFixed(2)}%)
-                </span>
+                <div>
+                  {parseFloat(data.lastPrice).toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                    maximumFractionDigits: 2,
+                  })}
+                </div>
+                <div
+                  className={
+                    priceMovementState === "UP"
+                      ? "text-green-500"
+                      : priceMovementState === "DOWN"
+                      ? "text-red-500"
+                      : "text-gray-500"
+                  }
+                >
+                  <span className="mr-2">
+                    {priceMovementState === "UP"
+                      ? "↗"
+                      : priceMovementState === "DOWN"
+                      ? "↘"
+                      : "N/A"}
+                  </span>
+                  <span>
+                    {parseFloat(data.priceChange).toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                      maximumFractionDigits: 4,
+                    })}
+                  </span>
+                  <span className="px-2">
+                    ({parseFloat(data.priceChangePercent).toFixed(2)}%)
+                  </span>
+                </div>
               </div>
             ))}
           </div>
         )}
-        <div ref={candlestickChartRef}></div>
-        <div ref={areaChartRef}></div>
+        <div className="grid grid-cols-2 gap-4">
+          <div ref={areaChartRef}></div>
+          <div ref={candlestickChartRef}></div>
+          <div ref={barChartRef}></div>
+          <div ref={baselineChartRef}></div>
+          <div ref={histogramChartRef}></div>
+          <div ref={lineChartRef}></div>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
